b90811190f7a04d23b160fb65a956f268194892b
Lucas Thurler Gonçalves <163908689+LucasThurler@users.noreply.github.com>
2026-05-06 21:57:19 -0300

    tem class dado

--- joguinho_do_Clue/src/model/Dado.java
+++ joguinho_do_Clue/src/model/Dado.java
@@ -1,2 +1,21 @@
 package model;
 
+import java.util.concurrent.ThreadLocalRandom;
+
+class Dado{
+    private int numFaces;
+    private int valorAtual;
+
+    Dado(int numFaces) {
+        this.numFaces = numFaces;
+    }
+
+    int lancar(){
+        valorAtual = ThreadLocalRandom.current().nextInt(1, numFaces + 1);
+        return valorAtual;
+    }
+
+    int getValorAtual() {
+        return valorAtual;
+    }
+}
